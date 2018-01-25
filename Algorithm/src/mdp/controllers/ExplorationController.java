8df0a7b99773c0d2a63ec1c8e33f2f067d0df16d
Tan Ying Hao <C150124@e.ntu.edu.sg>
2018-01-25 15:15:56 +0800

    TYH[] Edited ExplorationController to inherit from ExplorationBase class

--- Algorithm/src/mdp/controllers/ExplorationController.java
+++ Algorithm/src/mdp/controllers/ExplorationController.java
@@ -1,11 +1,19 @@
 package mdp.controllers;
 
+import mdp.robots.RobotBase;
+
 /**
  * ExplorationController is a ExplorationBase implementation that performs the actual
  * exploration by using a robot and understanding its environment states
  * 
  * @author Ying Hao
  */
-public class ExplorationController {
+public class ExplorationController extends ExplorationBase {
+
+	@Override
+	public void explore(RobotBase robot) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
