995f65436ac6dd0ea39ed141773a93c239646057
Gnoos <jieming1994@gmail.com>
2018-01-30 16:42:11 +0800

    blah

--- Arduino/Sensor/Sensor.ino
+++ Arduino/Sensor/Sensor.ino
@@ -3,9 +3,10 @@
 #define mfwdIr A0       // These three sensors represent the front portion of the arduino, A0 - A4 are all short range IRs, A5 is the only long range IR
 #define lfwdIr A1
 #define rfwdIr A2
-#define brgtIr A3
 #define frgtIr A4
-#define flftIr A5 
+#define flftIr A3
+#define brgtIr A5 
+
 #define shrtmodel 1080
 #define longmodel 20150
 // ir: the pin where your sensor is attached
@@ -38,22 +39,22 @@ void loop() {
   int dis6=ir6.distance();
 
   Serial.print("Mean distance of left fwd sensor: ");  // returns it to the serial monitor
-  Serial.println(dis1);
+  Serial.println(dis1/10);
 
   Serial.print("Mean distance of mid fwd sensor: ");  // returns it to the serial monitor
-  Serial.println(dis2);
+  Serial.println(dis2/10);
 
   Serial.print("Mean distance of right fwd sensor: ");  // returns it to the serial monitor
-  Serial.println(dis3);
+  Serial.println(dis3/10);
 
   Serial.print("Mean distance of back side sensor: ");  // returns it to the serial monitor
-  Serial.println(dis6);
+  Serial.println(dis6/10);
 
   Serial.print("Mean distance of front side sensor (short): ");  // returns it to the serial monitor
-  Serial.println(dis5);
+  Serial.println(dis5/10);
 
   Serial.print("Mean distance of front side sensor (long): ");  // returns it to the serial monitor
-  Serial.println(dis4);
+  Serial.println(dis4/10);
   
   unsigned long pepe2=millis()-pepe1;  // the following gives you the time taken to get the measurement
   Serial.print("Time taken (ms): ");
