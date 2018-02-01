8e91e5adce31b1e99ac3309385f1cda8e61218a1
Gnoos <jieming1994@gmail.com>
2018-02-01 21:43:54 +0800

    blah

--- Arduino/Sensor/Sensor.ino
+++ Arduino/Sensor/Sensor.ino
@@ -39,22 +39,22 @@ void loop() {
   int dis6=ir6.distance();
 
   Serial.print("Mean distance of left fwd sensor: ");  // returns it to the serial monitor
-  Serial.println(dis1/10);
+  Serial.println(dis1);
 
   Serial.print("Mean distance of mid fwd sensor: ");  // returns it to the serial monitor
-  Serial.println(dis2/10);
+  Serial.println(dis2);
 
   Serial.print("Mean distance of right fwd sensor: ");  // returns it to the serial monitor
-  Serial.println(dis3/10);
+  Serial.println(dis3);
 
   Serial.print("Mean distance of back side sensor: ");  // returns it to the serial monitor
-  Serial.println(dis6/10);
+  Serial.println(dis6);
 
   Serial.print("Mean distance of front side sensor (short): ");  // returns it to the serial monitor
-  Serial.println(dis5/10);
+  Serial.println(dis5);
 
   Serial.print("Mean distance of front side sensor (long): ");  // returns it to the serial monitor
-  Serial.println(dis4/10);
+  Serial.println(dis4);
   
   unsigned long pepe2=millis()-pepe1;  // the following gives you the time taken to get the measurement
   Serial.print("Time taken (ms): ");
