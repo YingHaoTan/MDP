503d323f6ad23862f3f726630b6bb0df050eaa7e
SGCOUH <SOHG0008@e.ntu.edu.sg>
2018-02-09 09:53:06 +0800

    Add files via upload

---
+++ RasberryPi/bluetooth/bluetoothclient.py
@@ -0,0 +1,14 @@
+#CLIENT SIDE
+import bluetooth
+
+android_addr = "08:60:6E:AD:33:FC"
+
+port = 1
+
+android_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+android_socket.connect((android_addr, port))
+s
+while(True):
+    android_socket.send("data...")
+    
+android_socket.close()
