da85387059222f5faa4821bcf6a453dc45fc79d7
Jing Yang <k.jingyang@gmail.com>
2018-04-10 10:40:18 +0800

    tested encoded cal

--- Algorithm/src/mdp/controllers/explorer/MovementBase.java
+++ Algorithm/src/mdp/controllers/explorer/MovementBase.java
@@ -200,12 +200,12 @@ public abstract class MovementBase {
             }
         }
         
-        /*
+        
         System.out.println("========== Obstacles Counter =============");
         printGrid(obstaclesCounter);
         System.out.println("========== No Obstacles Counter =============");
         printGrid(noObstaclesCounter);
-        */
+        
 
         for(Runnable listener: this.scanlisteners)
         	listener.run();
